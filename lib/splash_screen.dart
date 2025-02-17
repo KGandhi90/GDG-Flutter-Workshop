c92e8c3691a174fb813bb1632c3f4953b496b686
KGandhi90 <kevin.gandhi@somaiya.edu>
2025-02-17 23:56:42 +0530

    Workshop Day 1

---
+++ lib/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), (){
+      Navigator.pushReplacementNamed(context, '/start');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("I am Kevin.")));
+  }
+}
